d4d556881da32daa7df335e153dff9facf41b1c2
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-07-06 14:17:01 -0400

    add menu para empezar a dar estilos al mismo

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 function yamile_style(){
     wp_enqueue_style('normalize', get_stylesheet_directory_uri().'/css/normalize.css' );
-    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.css' );
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css' );
     wp_enqueue_style('style', get_stylesheet_uri());  
     wp_enqueue_script('jquery');  
-    wp_enqueue_script('bootstrapjs',get_stylesheet_directory_uri().'/js/bootstrap.js', array('jquery'),'1.4.1', true );  
+    wp_enqueue_script('bootstrapjs',get_stylesheet_directory_uri().'/js/bootstrap.min.js', array('jquery'),'1.4.1', true );  
     
 }
 
@@ -14,7 +14,7 @@ add_action('wp_enqueue_scripts','yamile_style');
 function register_my_menus(){
     
     register_nav_menus(array(
-        'principal' => __('Menu principal', 'yamilemakeup')
+        'principal' => __('Menu Principal', 'yamilemakeup')
     ));
 
 }
--- header.php
+++ header.php
@@ -9,31 +9,26 @@
 </head>
 <body>
     <header class="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light pt-1 pb-1">
-      <a class="navbar-brand" href="<?php echo home_url( '/' ); ?>">
-      <img alt="<?php bloginfo("name"); ?>" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo_clad.svg " class="d-inline-block align-top">
-        </a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <hr class="line line-nav-l d-none d-xl-block">  
-      
-      
-       <?php 
-
-
-       wp_nav_menu( array(
-          'theme_location'  => 'menu-principal',
-          'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
-          'container'       => 'div',
-          'container_class' => 'collapse navbar-collapse',
-          'container_id'    => 'navbarTogglerDemo01',
-          'menu_class'      => 'navbar-nav ml-1 text-center',
-          'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-          'walker'          => new WP_Bootstrap_Navwalker(),
-      ) );
-
-      ?>
+    <nav class="">
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a class="navbar-brand" href="<?php echo home_url( '/' ); ?>">
+                            <img alt="<?php bloginfo("name"); ?>" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo negro Yamile.png " width="200" class="d-inline-block align-top">
+                            </a>
+                    </div>
+                    <div class="col-md-8">
+                    
+                            <?php 
+                                wp_nav_menu( array(
+                                    'theme_location'  => 'principal',
+                                    'container_id'    => 'navbar',
+                                    'menu_class' => 'nav navbar-nav navbar-right',
+                                ) );
+                            ?>
+                    </div>
+                </div>
+        </div>
 
         <!-- <div class="container">
             <section class="header-logo-container">
